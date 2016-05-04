eede3e713cfce3889c2b1cb4cb082b0e143a5938
Ching <chingyang91@gmail.com>
2016-05-04 16:24:42 +0800

    NumToText function using recursion

--- numToText.js
+++ numToText.js
@@ -14,5 +14,42 @@ Ex: numToText("I have 5 dogs and 6 ponies"); // returns "I have five dogs and si
 */
 
 var numToText = function(str) {
+  var result = '';
+  var strArr = str.split(' ');
 
+  if (strArr.length > 1) {
+    for (var i = 0; i < strArr.length; i++) {
+      if (i === strArr.length - 1) {
+        result += numToText(strArr[i]);
+      } else {
+        result += numToText(strArr[i]) + ' ';
+      }
+    }
+  } else {
+    if (!isNaN(Number(str))) {
+      if (str === '1') {
+        result += 'one';
+      } else if (str === '2') {
+        result += 'two';
+      } else if (str === '3') {
+        result += 'three';
+      } else if (str === '4') {
+        result += 'four';
+      } else if (str === '5') {
+        result += 'five';
+      } else if (str === '6') {
+        result += 'six';
+      } else if (str === '7') {
+        result += 'seven';
+      } else if (str === '8') {
+        result += 'eight';
+      } else if (str === '9') {
+        result += 'nine';
+      }
+    } else {
+      result += str;
+    }
+  }
+  
+  return result;
 };
